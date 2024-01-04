ac227ca0aabff6db047ef3fb356a1ad9779922ba
bikriira <iradufashabikri@gmail.com>
2024-01-03 18:00:50 -0800

    Fix: The logic error found in the last condition in file 1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,5 +6,5 @@ if last > 5:
     print(f"Last digit of {number} is {last} and is greater than 5")
 elif last == 0:
     print(f"Last digit of {number} is {last} and is 0")
-elif 0 < last < 6:
+else:
     print(f"Last digit of {number} is {last} and is less than 6 and not 0")
