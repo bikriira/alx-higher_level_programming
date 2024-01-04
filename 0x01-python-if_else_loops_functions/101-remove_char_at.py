046675a893f8a9251689d2b882db7bb06ed0a9a9
bikriira <iradufashabikri@gmail.com>
2024-01-04 00:02:57 -0800

    Feat: a function that creates a copy of the string, removing the character at the position n (not the Python way, the “C array index”).

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    new_str = ""
+    counter = 0
+    if n < 0:
+        return str
+    for char in str:
+        if counter != n:
+            new_str += char
+        counter += 1
+    return new_str
+		
