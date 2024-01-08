0419c4c1b62a49b1184b0f284fdce9aa355bdf3f
bikriira <iradufashabikri@gmail.com>
2024-01-08 09:55:40 -0800

    Feat:  a function that replaces an element of a list at a specific position (like in C).

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return None
+    for i, value in enumerate(my_list):
+        if i == idx:
+            del my_list[idx]
+            my_list.insert(idx, element)
+            return my_list
