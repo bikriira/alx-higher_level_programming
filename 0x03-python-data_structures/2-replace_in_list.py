38d2123b236c5b68364d3c55ce9e9fe9cf525c14
bikriira <iradufashabikri@gmail.com>
2024-01-08 10:06:17 -0800

    Fix: is out of range (> of number of element in my_list), the function should not modify anything, and returns the original list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
+    if idx < 0:
         return None
+    if idx >= len(my_list):
+        return my_list
     for i, value in enumerate(my_list):
         if i == idx:
             del my_list[idx]
