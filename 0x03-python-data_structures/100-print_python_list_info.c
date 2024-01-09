ac39cb69e4322fd437b5472544fc0283baf857f5
bikriira <iradufashabikri@gmail.com>
2024-01-09 10:53:49 -0800

    Attack

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,25 @@
+#include <Python.h>
+
+
+/**
+ * print_python_list_info - function name
+ * @p: the bject
+ *
+ * Return: void
+ */
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t size_p, allocated, idx = 0;
+	PyObject *element;
+
+	size_p = PyList_Size(p);
+	allocated = ((PyListObject *)p)->allocated;
+	printf("[*] Size of the Python List = %ld\n", size_p);
+	printf("[*] Allocated = %ld\n", allocated);
+	while (idx < size_p)
+	{
+		element = PyList_GET_ITEM(p, idx);
+		printf("Element %ld: %s\n", idx, element->ob_type->tp_name);
+		idx++;
+	}
+}
