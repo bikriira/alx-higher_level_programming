ba9f8b3e6c8adb857f98887ad505c547e4c838b0
bikriira <iradufashabikri@gmail.com>
2024-01-12 12:13:51 -0800

    [Fix] Handle case passed 'XCIX'

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,26 +1,29 @@
-#!/usr/bin/python3
 def roman_to_int(roman_string):
     exact_value = 0
     my_dict = {
-        "M": 100, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1
-        }
+        "M": 100, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1, "U": 0
+    }
 
     if (not isinstance(roman_string, str)) or roman_string is None:
         return 0
+
+    roman_string = list(roman_string)
+
     if len(roman_string) == 1:
-        return my_dict.get(roman_string, "Unkown num_char")
+        return my_dict.get(roman_string[0], "Unkown num_char")
 
     for i in range(len(roman_string)):
+        print("working on ", roman_string[i])
         num = my_dict.get(roman_string[i], "Unkown num_char")
         if i < len(roman_string) - 1:
             num_next = my_dict.get(roman_string[i + 1], "Unkown num_char")
-        else:
-            num_next = 0
-
-        if num < num_next:
-            special_num = num_next - num
-            exact_value += special_num
-            break
+            if num < num_next:
+                if roman_string[i] == "U":
+                    continue
+                roman_string[i + 1] = "U"
+                special_num = num_next - num
+                exact_value += special_num
+                continue
 
         exact_value += num
 
