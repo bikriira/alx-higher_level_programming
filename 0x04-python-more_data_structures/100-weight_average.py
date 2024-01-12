f89580d6c1b3a8e43508f02efcbfaa3b02970a4f
bikriira <iradufashabikri@gmail.com>
2024-01-12 12:35:52 -0800

    [Fix] runtime errors

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    uper_statement = 0
+    base_statement = 0
     for left, right in my_list:
         uper_statement += left * right
         base_statement += right
