44b1e8d355406e5afa57ad3df5b10b0d52f630a0
bikriira <iradufashabikri@gmail.com>
2024-01-12 13:26:58 -0800

    [Fix] Pycodestyle

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if a_dictionary.get(key, None) == None:
+    if a_dictionary.get(key, None) is None:
         return a_dictionary
     del a_dictionary[key]
     return a_dictionary
