4f3086683cd5a00895dc4b6f740668d65db1a0b8
bikriira <iradufashabikri@gmail.com>
2024-01-22 05:14:52 -0800

    Trying to fix red checker

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,7 +5,7 @@ def list_division(my_list_1, my_list_2, list_length):
         result = 0
         try:
             result = my_list_1[i] / my_list_2[i]
-        except (ValueError, TypeError):
+        except (TypeError):
             print("wrong type")
         except ZeroDivisionError:
             print("division by 0")
