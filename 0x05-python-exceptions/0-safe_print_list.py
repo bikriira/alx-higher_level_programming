50ad31f3d483819c83a547e86450be4e5b38395d
bikriira <iradufashabikri@gmail.com>
2024-01-22 03:21:24 -0800

    [Fix] make the function return

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    count = 0
     for i in range(x):
         try:
             print(int(my_list[i]), end="")
         except (ValueError, IndexError):
             pass
+        else:
+            count += 1
+    print()
+    return count
