f021d8cae2e78a0805fe32cbb23f47ab6f81f6bf
bikriira <iradufashabikri@gmail.com>
2024-01-22 04:54:13 -0800

    [Fix] handle when a list is a matrix

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -5,7 +5,7 @@ def list_division(my_list_1, my_list_2, list_length):
         result = 0
         try:
             result = int(my_list_1[i]) / int(my_list_2[i])
-        except ValueError:
+        except (ValueError, TypeError):
             print("wrong type")
         except ZeroDivisionError:
             print("division by 0")
