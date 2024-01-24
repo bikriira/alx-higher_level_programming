bb4b1a64a899f48ebc295ade28319f96920e8bc9
bikriira <iradufashabikri@gmail.com>
2024-01-24 07:00:37 -0800

    [Fix]

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -58,11 +58,11 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
-        print("\n" * self.__position[1], end="")
-        for row in range(self.__size):
-            print(" " * self.__position[0], end="")
-            for col in range(self.__size):
-                print("#", end="", sep="")
+        if self.__size == 0:
             print()
-        if self.__size <= 0:
-            print()
+        else:
+            for _ in range(self.__position[1]):
+                print()
+
+            for _ in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
