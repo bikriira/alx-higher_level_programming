f1c63988d1c67671a71f3116ca15ceed9f13e088
Iradufasha Bikri <iradufashabikri@gmail.com>
2024-01-24 22:51:12 +0200

    [Fix] x/y would cause runtime error

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -84,9 +84,11 @@ class Square:
         if (not isinstance(value, tuple)) or len(value) != 2:
             raise TypeError('position must be a tuple of 2 positive integers')
         x, y = value
-        if not (x < isinstance(x, int) and y < isinstance(y, int)) or (x / y < 0):
+        if not (x < isinstance(x, int) and y < isinstance(y, int)):
             raise TypeError('position must be a tuple of 2 positive integers')
-        self.__position = value
+        if x < 0 or y < 0:
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = (x, y)
 
     def area(self):
         """Calulates area of square.
