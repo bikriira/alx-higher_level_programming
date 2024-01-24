039d9803d80c29ad49d99af6de4fe8ca2fae3d9b
bikriira <iradufashabikri@gmail.com>
2024-01-24 11:48:41 -0800

    [Feat] my own 6-square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,111 +1,56 @@
 #!/usr/bin/python3
-"""Square generation module for Python project 0x06
+"""
+    2-square.py
+    Defines class Square
 """
 
 
 class Square:
-    """Class defined for square generation.
+    """
+        Defines a square by: (based on 1-square.py)
 
-    Args:
-        size (int): length of one side of square
-        position (tuple) ((int), (int)): horizontal offset in spaces,
-        vertical offset in newlines
-
-
-    Attributes:
-        __size (int): length of one side of square
-        __position (tuple) ((int), (int)): horizontal offset in spaces,
-        vertical offset in newlines
-
-
+        Attributes:
+            size (int): size of the square
+            position ((int), (int)): as X and Y in math (x, y)
     """
     def __init__(self, size=0, position=(0, 0)):
-        # attribute assigment here engages setters defined below
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        """__size getter, setter with same method name
-
-        Returns:
-            __size (int): length of one side, squared
-
-        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Args:
-            value (int): length of one side of square
-
-        Attributes:
-            __size (int): length of one side of square
-
-        Raises:
-            TypeError: if value is not an integer
-            ValueError: if value is less than 0
-
-        """
-        if type(value) is not int:
-            raise TypeError('size must be an integer')
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
         if value < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = value
+            raise ValueError("size must be >= 0")
+        self.__size = int(value)
 
     @property
     def position(self):
-        """__position getter, setter with same method name
-
-        Returns:
-            __position (tuple) ((int), (int)): horizontal offset in spaces,
-            vertical offset in newlines
-
-        """
-        return self.__position
+        return self.__property
 
     @position.setter
     def position(self, value):
-        """Args:
-            value (int): tuple of two positive integers
-
-        Attributes:
-            __position (tuple) ((int), (int)): horizontal offset in spaces,
-            vertical offset in newlines
-
-        Raises:
-            TypeError: if value is not a tuple of two positive ints
-
-        """
-        if type(value) is not tuple:
-            raise TypeError('position must be a tuple of 2 positive integers')
-        if len(value) is not 2:
-            raise TypeError('position must be a tuple of 2 positive integers')
-        for num in value:
-            if type(num) is not int or num < 0:
-                raise TypeError('position must be a tuple of ' +
-                                '2 positive integers')
-        self.__position = value
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        x, y = value
+        if x < 0 or y < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = (x, y)
 
     def area(self):
-        """Calulates area of square.
-
-        Attributes:
-            __size (int): length of one side of square
-
-        Returns:
-            area (int): length of one side, squared
-
-        """
-        area = self.__size * self.__size
-        return area
+        return self.__size ** 2
 
     def my_print(self):
         print("\n" * self.__position[1], end="")
         for row in range(self.__size):
             print(" " * self.__position[0], end="")
             for col in range(self.__size):
-                print("#", end="", sep="")
+                print("#", end="")
             print()
         if self.__size <= 0:
             print()
