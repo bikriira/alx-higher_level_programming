f8699c3e31cce8c64854de97d3c629d15f926f24
bikriira <iradufashabikri@gmail.com>
2024-01-24 04:56:17 -0800

    [Fix] syntax error on line 23

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -20,7 +20,7 @@ class Square:
             Args:
                 size (int): size of the square
         """
-        if type(size) not int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
