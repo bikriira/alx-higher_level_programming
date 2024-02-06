a0ca1731d72117cb0468378de1d3d516a3bc6caa
bikriira <iradufashabikri@gmail.com>
2024-02-06 13:51:25 -0800

    [Feat] A script that adds all arguments to a Python list, and then save them to a file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module that loads, adds and saves arguments to a Python list"""
+import sys
+import json
+
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+try:
+    my_list = load_from_json_file("add_item.json")
+except (FileNotFoundError, json.JSONDecodeError):
+    with open("add_item.json", "w", encoding="utf-8") as file:
+        save_to_json_file([], "add_item.json")
+
+for i in range(1, len(sys.argv)):
+    my_list.append(sys.argv[i])
+    save_to_json_file(my_list, "add_item.json")
