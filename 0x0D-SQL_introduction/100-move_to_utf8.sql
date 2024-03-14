fda7f8cfde60a53de30558564a61e6639f9aba6c
bikriira <iradufashabikri@gmail.com>
2024-03-14 06:31:42 -0700

    [Tracing the problem]

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -6,5 +6,7 @@
 -- 		Field name in first_table
 
 
-USE hbtn_0c_0
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
+
+ALTER TABLE first_table
+	CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
