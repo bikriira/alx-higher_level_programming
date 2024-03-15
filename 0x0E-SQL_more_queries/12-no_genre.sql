8f19adc8daa587c0729c8c0ee5c4f0e5130f49df
bikriira <iradufashabikri@gmail.com>
2024-03-15 14:39:21 -0700

    [Fix] file mix

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,4 +1,4 @@
--- A script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- A script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
 
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
@@ -6,7 +6,9 @@
 -- The database name will be passed as an argument of the mysql command
 
 
-SELECT tv_shows.title, tv_show_genres.genre_id
-	FROM tv_shows
-	RIGHT JOIN tv_show_genres
-		ON tv_show_genres.show_id = tv_shows.id
+SELECT b.title, a.genre_id
+    FROM tv_show_genres a
+	RIGHT JOIN tv_shows b
+        ON a.show_id = b.id
+    WHERE a.genre_id IS NULL
+	ORDER BY b.title, a.genre_id;
