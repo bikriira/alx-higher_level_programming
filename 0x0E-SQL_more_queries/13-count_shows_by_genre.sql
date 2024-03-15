bb7882b627c77d9b40b29996b141c5c4f4bbbb1b
bikriira <iradufashabikri@gmail.com>
2024-03-15 15:13:29 -0700

    [Fix] use -- for commenting

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,19 +1,19 @@
-/*
-Description:
-This script lists all genres from the hbtn_0d_tvshows database and displays the number of shows linked to each genre.
+-- Description:
+-- This script lists all genres from the hbtn_0d_tvshows database
+-- 		and displays the number of shows linked to each genre.
 
-Column Names:
-- genre: TV Show genre
-- number_of_shows: Number of shows linked to this genre
+-- Column Names:
+-- - genre: TV Show genre
+-- - number_of_shows: Number of shows linked to this genre
 
-Criteria:
-- First column must be called genre
-- Second column must be called number_of_shows
-- Don’t display a genre that doesn’t have any shows linked
-- Results must be sorted in descending order by the number of shows linked
-- You can use only one SELECT statement
-- The database name will be passed as an argument of the mysql command
-*/
+-- Criteria:
+-- - First column must be called genre
+-- - Second column must be called number_of_shows
+-- - Don’t display a genre that doesn’t have any shows linked
+-- - Results must be sorted in descending order by the number of shows linked
+-- - You can use only one SELECT statement
+-- - The database name will be passed as an argument of the mysql command
+
 
 SELECT
     tv_genres.name,
