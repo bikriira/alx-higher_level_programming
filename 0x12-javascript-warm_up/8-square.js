43ae8777ffdbf87b4779ad9c4687b2db33fd7a1e
bikriira <bikriiradufasha@gmail.com>
2024-05-03 16:51:52 +0200

    [Fix] use isNaN for checking

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 
-const process = require('process');
+const process = require("process");
 const rounds = process.argv[2];
 
-if (rounds !== undefined) {
+if (isNaN(rounds)) {
+  console.log("Missing size");
+} else {
   for (let index = 0; index < rounds; index++) {
-    let row = '';
+    let row = "";
     for (let j = 0; j < rounds; j++) {
-      row += 'X';
+      row += "X";
     }
     console.log(row);
   }
-} else {
-  console.log('Missing size');
 }
