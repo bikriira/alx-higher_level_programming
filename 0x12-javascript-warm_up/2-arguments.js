a760c523daaa8c3c78d82dfa474bc85fe132ebdb
bikriira <bikriiradufasha@gmail.com>
2024-05-03 15:16:05 +0200

    [Fix] semistandard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
-const process = require("process");
-let argCount = process.argv.length;
+const process = require('process');
+const argCount = process.argv.length;
 
 if (argCount <= 2) {
-    console.log('No argument');
-}else if (argCount === 3) {
-    console.log('Argument found');
-}
-else{
-    console.log('Arguments found');
+  console.log('No argument');
+} else if (argCount === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
