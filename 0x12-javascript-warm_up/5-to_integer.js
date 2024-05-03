2f85dcaa5c0535c2b4a84308c9bc8c79ab489c6b
bikriira <bikriiradufasha@gmail.com>
2024-05-03 15:51:18 +0200

    [Feat] do task 6

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const process = require('process');
+const myArg = process.argv[2];
+
+if (isNaN(myArg)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ', parseInt(myArg));
+}
