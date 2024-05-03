9c64abe164ebe2b2f004e658c94f8f0d945d993b
bikriira <bikriiradufasha@gmail.com>
2024-05-03 16:53:59 +0200

    [Fix] semistandard

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 
-const process = require("process");
+const process = require('process');
 const rounds = process.argv[2];
 
 if (isNaN(rounds)) {
-  console.log("Missing size");
+  console.log('Missing size');
 } else {
   for (let index = 0; index < rounds; index++) {
-    let row = "";
+    let row = '';
     for (let j = 0; j < rounds; j++) {
-      row += "X";
+      row += 'X';
     }
     console.log(row);
   }
