c144d74a0db2556112cb990ec6ce3e0145efd1a0
bikriira <bikriiradufasha@gmail.com>
2024-05-06 14:34:13 +0200

    [Fix] .sort() works well only with strings, with int more tweaks are needed

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,10 +4,9 @@ let args = require('process').argv;
 
 if (args.length > 3) {
   args = args.slice(2);
-  console.log(args);
-  args.sort();
-  console.log(args);
-  console.log(args[args.length - 2]);
+  args = args.map(num => parseInt(num)); // Parse strings to integers
+  args.sort((a, b) => a - b); // Custom comparison function to sort numbers
+  console.log(args[args.length - 2]); // Print the second largest number
 } else {
   console.log('0');
 }
