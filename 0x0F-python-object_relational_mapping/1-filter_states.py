25c4e08b22ab3bdb814e1bcb65924a68c0d98263
bikriira <bikriiradufasha@gmail.com>
2024-05-16 07:39:22 +0200

    [Achievement] do task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+This script retrieves and prints all states 
+    with a name starting with N (upper N) from a MySQL database.
+"""
+import MySQLdb
+import sys
+
+
+def selector():
+    """
+    Lists all states with a name starting with N (upper N) 
+        from the database passed through the command line.
+
+    The database credentials (username, password, and database name)
+    should be passed as command line arguments (in that order):
+
+    Example usage:
+        ./0-select_states.py root "" hbtn_0e_0_usa
+
+    Raises:
+        MySQLdb.Error: An error occurred while interacting with the database.
+    """
+    try:
+        # Connect to the database
+        conn = MySQLdb.connect(
+            host="localhost", port=3306,
+            user=sys.argv[1],
+            password=sys.argv[2],
+            db=sys.argv[3]
+            )
+
+        cursor = conn.cursor()
+
+        # Execute the query to fetch all states ordered by id
+        cursor.execute(f"""SELECT *
+                          FROM states
+                          WHERE name LIKE BINARY 'N%'
+                          ORDER BY id ASC""")
+        results = cursor.fetchall()
+
+        # Print each row in the results
+        for row in results:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print(f"Error: {e}")
+
+    finally:
+        # Ensure the cursor and connection are properly closed
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+
+
+if __name__ == "__main__":
+    selector()
