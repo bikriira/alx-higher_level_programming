7c4f29046a2bd83acf363d2674be9705fea9c8d9
bikriira <bikriiradufasha@gmail.com>
2024-05-17 10:17:47 +0200

    [Achievement] do task 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Script that prints the first State object from the database using SQLAlchemy
+"""
+if __name__ == "__main__":
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+    from sqlalchemy.engine.url import URL
+
+    db_config = {
+        "drivername": "mysql+mysqldb",
+        "host": "localhost",
+        "port": 3306,
+        "username": sys.argv[1],
+        "password": sys.argv[2],
+        "database": sys.argv[3],
+    }
+
+    url = URL(**db_config, query={})
+    engine = create_engine(url, pool_pre_ping=True)
+
+    Base.metadata.create_all(bind=engine)  # create db&tables if not present
+    Session = sessionmaker(bind=engine)  # create Session class
+    session = Session()  # createsession object
+    result = session.query(State).first()
+    print(f"{result.id}: {result.name}")
