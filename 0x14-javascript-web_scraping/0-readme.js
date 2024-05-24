26f54cd2221cd93f472c12d9b68a3b24929304c9
bikriira <bikriiradufasha@gmail.com>
2024-05-24 16:03:10 +0200

    [Achievement] do task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+const fs = require('fs').promises;
+const fileName = require('process').argv[2];
+
+async function readFile (fileName) {
+  try {
+    const fileContent = await fs.readFile(fileName, 'utf-8');
+    console.log(fileContent);
+  } catch (error) {
+    console.log(error);
+  }
+}
+readFile(fileName);
