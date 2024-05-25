1ea73af6cf0821d011bcdd5942d9c21fd8ab732a
bikriira <bikriiradufasha@gmail.com>
2024-05-25 10:20:25 +0200

    [Fix] try fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,4 +1,17 @@
 #!/usr/bin/node
+
+const request = require('request');
+request(process.argv[2], (error, response, body) => {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    const occurrence = results.reduce((count, movie) => {
+      return count + (movie.characters.find((character) => character.endsWith('/18/')) ? 1 : 0);
+    }, 0);
+
+    console.log(occurrence);
+  }
+});
+
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (!error) {
