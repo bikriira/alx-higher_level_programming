3bce4ed0fd3113e598f4a14689f5c22168f426b3
bikriira <bikriiradufasha@gmail.com>
2024-05-25 14:24:49 +0200

    [Fix] check the statusCode'

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,19 +6,16 @@ const params = require('process').argv;
 const completedTodos = {};
 
 request(params[2], (error, response, body) => {
-  if (!error) {
+  if (!error & response.statusCode === 200) {
     const todos = JSON.parse(body);
     todos.forEach(currentTodo => {
       if (currentTodo.completed) {
         if (!completedTodos[currentTodo.userId]) {
           completedTodos[currentTodo.userId] = 0;
-          console.log('dfgf');
         }
         completedTodos[currentTodo.userId] += 1;
       }
     });
     console.log(completedTodos);
-  } else {
-    console.log(error);
   }
 });
