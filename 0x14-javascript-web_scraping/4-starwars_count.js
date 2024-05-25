4cfda77030bc0a336b68cbcff0e4217b5100dce6
bikriira <bikriiradufasha@gmail.com>
2024-05-25 09:15:21 +0200

    [Fix] try fix

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,19 @@
 #!/usr/bin/node
 
 const promisify = require('util').promisify;
-let request = require('request');
-request = promisify(request);
-
-const urlParam = require('process').argv[2];
+const request = promisify(require('request'));
 
 (async () => {
   try {
-    const response = await request(urlParam);
+    const response = await request(process.argv[2]);
     const movies = JSON.parse(response.body).results;
 
     const occurrence = movies.reduce((count, movie) => {
       return count + (movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/') ? 1 : 0);
     }, 0);
+
     console.log(occurrence);
-  } catch (error) {}
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
 })();
