dc1cd9631d441ae32fb92c8b802a2f9888c18419
bikriira <bikriiradufasha@gmail.com>
2024-05-25 08:54:30 +0200

    [Fix] handle invalid url

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,11 +7,13 @@ request = promisify(request);
 const urlParam = require('process').argv[2];
 
 (async () => {
-  const response = await request(urlParam);
-  const movies = JSON.parse(response.body).results;
+  try {
+    const response = await request(urlParam);
+    const movies = JSON.parse(response.body).results;
 
-  const occurrence = movies.reduce((count, movie) => {
-    return count + (movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/') ? 1 : 0);
-  }, 0);
-  console.log(occurrence);
+    const occurrence = movies.reduce((count, movie) => {
+      return count + (movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/') ? 1 : 0);
+    }, 0);
+    console.log(occurrence);
+  } catch (error) {}
 })();
